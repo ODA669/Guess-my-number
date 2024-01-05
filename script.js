6c910ff231fb730fecb16e08bcb21a73a1c9a708
oda <odair.martinez@pneusbelisle.com>
2024-01-05 16:57:06 -0500

    update

--- script.js
+++ script.js
@@ -13,15 +13,41 @@ console.log(document.querySelector('.guess').value);
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
 */
-const x = function () {
-  console.log(23);
-};
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+//if we erase this line the guest number disapier  and only show in the screen the question mark
+document.querySelector('.number').textContent = secretNumber;
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
-    document.querySelector('.message').textContent = 'No Number ⛔⛔! ';
+    document.querySelector('.message').textContent = '⛔ No Number! ⛔ ';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '👍 Correct Number!  ';
+    // score = score + 1;
+    score++;
+    document.querySelector('.score').textContent = score;
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High';
+      //score = score - 1;
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = ' You lost the game.';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low';
+      //score = score - 1;
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = ' You lost the game.';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
