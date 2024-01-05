e3d5a262dd4a5d5873310fdbe17e5b3da27051f5
oda <odair.martinez@pneusbelisle.com>
2024-01-05 10:41:56 -0500

    add code

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Correct Number!';
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+console.log(document.querySelector('.number').textContent);
+document.querySelector('.score').textContent = 20;
+console.log(document.querySelector('.score').textContent);
+
+console.log(document.querySelector('.guess').value);
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+const x = function () {
+  console.log(23);
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No Number ⛔⛔! ';
+  }
+});
